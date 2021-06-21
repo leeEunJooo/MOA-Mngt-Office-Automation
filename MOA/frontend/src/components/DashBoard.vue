04f2929fc4c78dfa2e54e60f9dc551cf324a49e5
eunju <dmswn334000@naver.com>
2021-06-22 00:27:50 +0900

    chart 영역 나누기

--- MOA/frontend/src/components/DashBoard.vue
+++ MOA/frontend/src/components/DashBoard.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-      
+  <div class="dashboard">
+      <div class="dashboard-content">
+        <!--제목-->
+        <div class="title-section">DashBoard</div>
+        <!--그래프-->
+        <div class="chart-section"></div>
+      </div>
   </div>
 </template>
 
@@ -11,5 +16,21 @@ export default {
 </script>
 
 <style>
+  .dashboard{
+    width:100%;
+    padding:70px 70px 0px 70px;
+    height: fit-content;
+  }
+  .dashboard .title-section{
+    width:100%;
+    height:fit-content !important;
+    font-family: GmarketSansBold !important;
+    font-size: 27px !important;
+    font-weight: bold;
+    line-height: 0.14;
+    letter-spacing: 0.04px;
+    text-align: left;
+    color: #575757;
+  }
 
 </style>
