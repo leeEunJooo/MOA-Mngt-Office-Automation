efd0fac1ea761fe5f177192d15898d3fcec74112
최민혜 <ckatjdeks@gmail.com>
2021-06-24 17:02:40 +0900

    수정

--- MOA/backend/routes/mlist.js
+++ MOA/backend/routes/mlist.js
@@ -31,43 +31,14 @@ connection.connect(function (err) {
       res.send(rows);
     });
   });
+  
+  //목록 검색
 
   //목록 등록
-router.post('/insertList',function(req,res){
-  console.log("목록 등록");
-  const list = {
-    'CUST_IDFY_SEQ': req.body,
-    'SROC_FILE_PATH_NM': "",
-    'LANG_CD': "",
-    'SYS_DIV_CD': "",
-    'CYCL_DATE_TYPE_CD': "",
-    'DOW_NM': "",
-    'DATA_EXE_TIME': "",
-    'RPY_RESLT_CD': "",
-    'TROBL_SVC_TYPE_CD': "",
-    'INPUT_VAL':"",
-    'TRT_STEP_NM': "",
-    'CONN_EVN_DIV_CD':"",
-    'ATTEN_MTR_SBST': "",
-    'ATC_FILE_MANUAL_YN': "",
-    'ATC_FILE_UPLD_PATH_NM': "",
-    'OTPUT_SBST': "",
-    'ETC_SBST': "",
-    'EXE_SBST': "",
-    'NTCART_TITLE_NM': "",
-    'TKCGR_NM': "",
-    'RUSER_NM': "",
-    'FIRST_REG_DATE': "",
-    'CHG_DATE':"",
-    'FNS_DATE': "",
-  }
-  connection.query('select ')
-})
 
 
   //목록 수정
 
-  //목록 검색
 
   
 module.exports = router;
